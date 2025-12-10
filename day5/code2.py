1dc5c01c841674c9218dbd520a051b30be88d201
Alexandre <alexandre.venturi@outlook.com>
2025-12-10 20:11:12 +0100

    day 5 - 2 done

--- day5/code2.py
+++ day5/code2.py
@@ -17,12 +17,43 @@ def preprocess_data():
                     
     return data[0]
 
+def mix_range():
+    idx = 0
+    while idx<len(input)-1:
+        if input[idx][0] == input[idx+1][0]:
+            if input[idx][1]<=input[idx+1][1]:
+                input.pop(idx)
+            else :
+                input.pop(idx+1)
+        elif input[idx+1][1] >= input[idx][1] >= input[idx+1][0]:
+            input[idx][1]=input[idx+1][1]
+            input.pop(idx+1)
+        elif input[idx][1]==input[idx+1][0]-1:
+            input[idx][1]=input[idx+1][1]
+            input.pop(idx+1)
+        elif input[idx][1] > input[idx+1][1]:
+            input.pop(idx+1)
+        else:
+            idx += 1
+    return input
+
 input = preprocess_data()
+input = sorted(input, key=lambda x: x[0])
+
+for i in range(len(input)-1):
+    if input[i][0]>input[i+1][0]:
+        print(False)
+
+input = mix_range()
+
+for i in range(len(input)-1):
+    if input[i][0]>input[i+1][0]:
+        print(False)
 
 print(input)
+s=0
+for r in input:
+    a=r[1]-r[0]+1
+    s+=a
 
-a = set()
-
-for i in input:
-    print(i[0]," ",i[1])
-print(len(a))
+print(s, s==354226555270043)
